21f40daf680c17b3bff385a8bb4f5a8dbe0c74d0
jedidiah musa <jedidiahsimon3@gmail.com>
2025-04-25 11:09:03 +0100

    .

--- father-williams/src/components/Contact.jsx
+++ father-williams/src/components/Contact.jsx
@@ -23,22 +23,8 @@ function Contact() {
     }
   };
   return (
-    <div className=" w-full flex flex-col lg:h-screen p-4">
-      <p className="w-full h-[4rem] font-[Anton] flex items-center justify-center text-4xl md:text-5xl ">
-        CONTACT US
-      </p>
-      <p className="text-2xl ">
-        Father Williams Abba Charitable Trust 14301 N. 87 St., Ste. 208 th
-        Scottsdale, AZ 85260 480-584-3517
-      </p>
-      <p className="underline cursor-pointer text-blue-900 text-2xl">info@abbacharitabletrust.org</p>
-      <div className="w-full flex items-center justify-center">
-        <button className="bg-[#2f5ec4] text-white flex w-full md:w-[50%] hover:scale-102 md:text-[1.1rem] font-bold py-5 px-4 items-center justify-center rounded-full mt-4">DOWNLOAD SCHOLARSHIP FORMS</button>
-      </div>
-
-
       <Carousel
-            className=" my-4 flex-1 w-full"
+            className=" my-4 flex-1  w-full"
               swipeable={true}
               draggable={true}
               responsive={responsive}
@@ -159,7 +145,6 @@ function Contact() {
               
             </Carousel>
       
-    </div>
   );
 }
 
--- father-williams/src/components/Footer.jsx
+++ father-williams/src/components/Footer.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import React from "react";
 
 function Footer() {
   return (
-    <div className="">
-       
+    <>
+      <div className="bg-black text-white">
+        <div className=" w-full flex flex-col  p-4">
+          <p className="w-full h-[4rem] font-[Anton] flex items-center justify-center text-4xl md:text-5xl ">
+            CONTACT US
+          </p>
+          <p className="text-2xl ">
+            Father Williams Abba Charitable Trust 14301 N. 87 St., Ste. 208 th
+            Scottsdale, AZ 85260 480-584-3517
+          </p>
+          <p className="underline cursor-pointer text-blue-900 text-2xl">
+            info@abbacharitabletrust.org
+          </p>
+          <div className="w-full flex items-center justify-center">
+            <button className="bg-[#2f5ec4] text-white flex w-full md:w-[50%] hover:scale-102 md:text-[1.1rem] font-bold py-5 px-4 items-center justify-center rounded-full mt-4">
+              DOWNLOAD SCHOLARSHIP FORMS
+            </button>
+          </div>
+        </div>
       </div>
-  )
+    </>
+  );
 }
 
-export default Footer
+export default Footer;
